34b1b2995db2380f7249923cffb2669b4f3805b3
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-10-06 15:22:16 +0530

    Method Calling

---
+++ src/day28/InternalMethodCalling.java
@@ -0,0 +1,24 @@
+package day28;
+
+public class InternalMethodCalling {
+
+    public void m1()
+    {
+        System.out.println("m1 method...");
+    }
+
+    public void m2()
+    {
+        System.out.println("m1 method...");
+    }
+
+    public void m3()
+    {
+        System.out.println("m1 method...");
+    }
+
+    public void m4()
+    {
+        System.out.println("m1 method...");
+    }
+}
